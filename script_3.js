64ae81065bb15bea717bda1abff8dddcb6edc41d
Alpha <ravalasonn@gmail.com>
2019-08-22 21:09:37 +0300

    Pyramide de Mario

--- script_3.js
+++ script_3.js
@@ -1,7 +1,19 @@
 let number = prompt("Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?")
 
 function my_half_pyramid(number) {
-	
+
+	for(var i = 1; i <= number; i++){
+		vide = "";
+		etoile = "";
+		for(var j = 1; j <= (number-i); j++){
+			vide = vide + " ";
+		}
+		for(var k = number-i; k < number; k++){
+			etoile = etoile + "*";
+		}
+		pyramide = vide + etoile;
+		console.log(pyramide);
+	}
 }
 
 my_half_pyramid(number)
